a8b58780e2e94805e2892497bea7415972dda8ec
James Dolezal <jamesmdolezal@gmail.com>
2024-08-12 12:58:37 -0500

    Add license information

--- slideflow_extras/clam/legacy/create_attention.py
+++ slideflow_extras/clam/legacy/create_attention.py
@@ -1,4 +1,24 @@
-"""Modification of https://github.com/mahmoodlab/CLAM"""
+# Slideflow-Extras - Add-ons for the deep learning library Slideflow
+# Copyright (C) 2024 James Dolezal
+#
+# This file is part of Slideflow-Extras.
+#
+# Slideflow-Extras is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Slideflow-Extras is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Slideflow-Extras. If not, see <https://www.gnu.org/licenses/>.
+#
+# This file incorporates work from CLAM, which is licensed
+# under the GNU General Public License, Version 3. The original
+# license and source code can be found at https://github.com/mahmoodlab/CLAM.
 
 import argparse
 import csv
--- slideflow_extras/clam/legacy/trainer.py
+++ slideflow_extras/clam/legacy/trainer.py
@@ -1,3 +1,21 @@
+# Slideflow-Extras - Add-ons for the deep learning library Slideflow
+# Copyright (C) 2024 James Dolezal
+#
+# This file is part of Slideflow-Extras.
+#
+# Slideflow-Extras is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Slideflow-Extras is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Slideflow-Extras. If not, see <https://www.gnu.org/licenses/>.
+
 """Legacy trainer for CLAM models."""
 
 import os
