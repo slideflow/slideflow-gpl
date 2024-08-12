c764d20ea22159d0bba90bbf45e8aaeb1fb4f7b5
James Dolezal <jamesmdolezal@gmail.com>
2024-08-12 08:43:04 -0500

    Fix CLAM model registration

--- slideflow_extras/clam/__init__.py
+++ slideflow_extras/clam/__init__.py
@@ -7,22 +7,22 @@ from slideflow.mil import register_trainer, register_model
 def legacy_clam():
     return LegacyCLAMTrainerConfig
 
-@register_model
+@register_model(config=CLAMModelConfig)
 def clam_sb():
     from .model import CLAM_SB
     return CLAM_SB
 
-@register_model
+@register_model(config=CLAMModelConfig)
 def clam_mb():
     from .model import CLAM_MB
     return CLAM_MB
 
-@register_model
+@register_model(config=CLAMModelConfig)
 def mil_fc():
     from .model import MIL_fc
     return MIL_fc
 
-@register_model
+@register_model(config=CLAMModelConfig)
 def mil_fc_mc():
     from .model import MIL_fc_mc
     return MIL_fc_mc
